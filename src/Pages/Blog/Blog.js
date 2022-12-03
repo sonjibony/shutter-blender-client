9f18fd9d6808de1752c8a2aa46c3c1c021f2425b
sonjibony <sonjibony@gmail.com>
2022-12-03 14:13:04 +0600

    blog section updated

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -4,92 +4,113 @@ import useTitle from "../../hooks/useTitle";
 const Blog = () => {
   useTitle("Blog");
   return (
-    <div className="my-7 w-3/4 mx-auto">
+    <div className="my-7 w-11/12 mx-auto">
       <div>
-        <h1 className="text-5xl mb-4">WELCOME</h1>
-        <h1 className="text-2xl mb-7">
-          Hope you find what you are looking for.
-        </h1>
+        <h1 className="text-5xl mb-4">THE BLOG</h1>
+        <h1 className="text-xl mb-7">
+        Stay informed with the latest news, gear and technology on the Outdoor Photographer blog, featuring guest posts from pro photographers and spotlighting images from the <span className="text-green-500">Shutter Blender</span> community.        </h1>
       </div>
-      <div
-        tabIndex={0}
-        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded mb-4"
-      >
-        <div className="collapse-title text-xl font-medium">
-          What are the differences between SQL and NoSQL?{" "}
-        </div>
-        <div className="collapse-content">
+      <div className="card lg:card-side bg-base-100  rounded-none">
+        <figure>
+          <img
+            src="https://images.pexels.com/photos/11388549/pexels-photo-11388549.jpeg?cs=srgb&dl=pexels-stshering-11388549.jpg&fm=jpg&h=400&w=550&fit=crop&_gl=1*16pods*_ga*NDAwNTIyNDM1LjE2NTg5OTQ3NzI.*_ga_8JE65Q40S6*MTY3MDA1MDAxOC4yNi4xLjE2NzAwNTAxMDguMC4wLjA."
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Finding Inspiration In Our National Parks Assignment Winner Denis
+            Dessoliers
+          </h2>
+          <p className="text-sm text-gray-500">December 1, 2022 | By Staff</p>
           <p>
-            SQL databases are relational, NoSQL databases are non-relational.
-            SQL databases use structured query language and have a predefined
-            schema. NoSQL databases have dynamic schemas for unstructured data.
-            SQL databases are vertically scalable, while NoSQL databases are
-            horizontally scalable. SQL databases are table-based, while NoSQL
-            databases are document, key-value, graph, or wide-column stores. SQL
-            databases are better for multi-row transactions, while NoSQL is
-            better for unstructured data like documents or JSON.
+            Congratulations to Denis Dessoliers for winning the recent Finding
+            Inspiration In Our National Parks Assignment with the image, “Zion
+            Wonderland.”...
           </p>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-outline btn-success btn-sm rounded">
+              Read More
+            </button>{" "}
+          </div>
         </div>
       </div>
-      <div
-        tabIndex={1}
-        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded mb-4"
-      >
-        <div className="collapse-title text-xl font-medium">
-          What is JWT, and how does it work?
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6 mt-11">
+        <div className="card card-compact w-96 bg-base-100 rounded-none">
+          <figure>
+            <img
+              src="https://images.pexels.com/photos/797797/pexels-photo-797797.jpeg?cs=srgb&dl=pexels-vasanth-babu-797797.jpg&fm=jpg&h=400&w=550&fit=crop&_gl=1*1wm1wu2*_ga*NDAwNTIyNDM1LjE2NTg5OTQ3NzI.*_ga_8JE65Q40S6*MTY3MDA1MjI2NC4yNy4xLjE2NzAwNTI0MzguMC4wLjA."
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Photo Of The Day By Rom Savage</h2>
+            <p className="text-sm text-gray-500">December 1, 2022 | By Staff</p>
+            <p>
+              Today’s Photo of the Day is “Enchanting Transformation” by Rom
+              Savage. Location: Roxborough, Colorado. “Sometimes when nature is
+              feeling generous,...
+            </p>
+            <div className="card-actions justify-start mt-3 ">
+              <button className="btn btn-outline btn-success btn-sm rounded">
+                Read More
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="collapse-content">
-          <p>
-            JSON Web Token (JWT) is an open standard (RFC 7519) that defines a
-            compact and self-contained way for securely transmitting information
-            between parties as a JSON object. This information can be verified
-            and trusted because it is digitally signed. Authentication server
-            verifies the credentials and issues a jwt signed using either a
-            secret salt or a private key. User's Client uses the JWT to access
-            protected resources by passing the JWT in HTTP Authorization header.
-            Resource server then verifies the authenticity of the token using
-            the secret salt/ public key.
-          </p>
+        <div className="card card-compact w-96 bg-base-100 rounded-none">
+          <figure>
+            <img
+              src="https://images.pexels.com/photos/2258536/pexels-photo-2258536.jpeg?cs=srgb&dl=pexels-frank-cone-2258536.jpg&fm=jpg&h=400&w=550&fit=crop&_gl=1*6rr9q0*_ga*NDAwNTIyNDM1LjE2NTg5OTQ3NzI.*_ga_8JE65Q40S6*MTY3MDA1MjI2NC4yNy4xLjE2NzAwNTI5NDIuMC4wLjA."
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Behind The Shot: Earth’s Shadow At Thunder
+            </h2>
+            <p className="text-sm text-gray-500">
+              November 23, 2022 | By Staff
+            </p>
+            <p>
+              This photograph was 23 years in the making. It was inspired by a
+              powerful series of photographs that archetypal mountain
+              photographer Galen Rowell made from the summit of a minor peak in
+              Nepal’s Everest region and published...
+            </p>
+            <div className="card-actions justify-start mt-3 ">
+              <button className="btn btn-outline btn-success btn-sm rounded">
+                Read More
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        tabIndex={2}
-        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded mb-4"
-      >
-        <div className="collapse-title text-xl font-medium">
-          What is the difference between Javascript and NodeJs?
+        <div className="card card-compact w-96 bg-base-100 rounded-none">
+          <figure>
+            <img
+              src="https://images.pexels.com/photos/385976/pexels-photo-385976.jpeg?cs=srgb&dl=pexels-nubia-navarro-%28nubikini%29-385976.jpg&fm=jpg&h=400&w=550&fit=crop&_gl=1*1raeuyj*_ga*NDAwNTIyNDM1LjE2NTg5OTQ3NzI.*_ga_8JE65Q40S6*MTY3MDA1MjI2NC4yNy4xLjE2NzAwNTMxODcuMC4wLjA."
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              High-Flying Photo Ops Assignment Winner Johnny Fermanides
+            </h2>
+            <p className="text-sm text-gray-500">October 14, 2022 | By Staff</p>
+            <p>
+              Congratulations to Johnny Fermanides for winning the recent
+              High-Flying Photo Ops Assignment with the image, “Shadowland.”
+              View the winning image...
+            </p>
+            <div className="card-actions justify-start mt-3 ">
+              <button className="btn btn-outline btn-success btn-sm rounded">
+                Read More
+              </button>
+            </div>
+          </div>
         </div>
-        <div className="collapse-content">
-          <p>
-            NodeJS is a cross-platform and open source Javascript runtime
-            environment that allows the javascript to be run on the server-side.
-            Nodejs allows Javascript code to run outside the browser. Nodejs
-            comes with a lot of modules and mostly used in web development. On
-            the other hand, Javascript is a Scripting language. It is mostly
-            abbreviated as JS. It can be said that Javascript is the updated
-            version of the ECMA script. Javascript is a high-level programming
-            language that uses the concept of Oops but it is based on prototype
-            inheritance.
-          </p>
-        </div>
-      </div>
-      <div
-        tabIndex={3}
-        className="collapse collapse-arrow border border-base-300 bg-base-100 rounded"
-      >
-        <div className="collapse-title text-xl font-medium">
-          How does NodeJS handle multiple requests at the same time?{" "}
-        </div>
-        <div className="collapse-content">
-          <p>
-            {" "}
-            NodeJS receives multiple client requests and places them into
-            EventQueue. NodeJS is built with the concept of event-driven
-            architecture. NodeJS has its own EventLoop which is an infinite loop
-            that receives requests and processes them.
-          </p>
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
